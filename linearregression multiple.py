4bdc5561f87ce80c4ee358c67ce242c15a1cf8f4
vamsi046 <70460687+vamsi046@users.noreply.github.com>
2021-04-10 10:48:15 +0530

    Create linearregression multiple.py

---
+++ linearregression multiple.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the diabetes dataset
+diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+
+print (diabetes_X)
+
+# Split the data into training/testing sets
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
+
+# Split the targets into training/testing sets
+diabetes_y_train = diabetes_y[:-20]
+diabetes_y_test = diabetes_y[-20:]
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+# Make predictions using the training set
+diabetes_y_pred = regr.predict(diabetes_X_train)
+
+# Make predictions using the testing set
+diabetes_y_pred = regr.predict(diabetes_X_test)
+
+# Make predictions using the testing set
+diabetes_y_pred = regr.predict(diabetes_X_test)
+# Use score method to get accuracy of model
+score = regr.score(diabetes_X_test, diabetes_y_test)
+print(score)
